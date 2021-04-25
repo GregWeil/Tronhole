e97e698f200c83418dda4775df027006f4ae3723
Greg Weil <bailinbone15@gmail.com>
2021-04-24 22:28:11 -0400

    Reworked obstacle spawning, use new prefabs

--- Assets/ObstacleSpawner.cs
+++ Assets/ObstacleSpawner.cs
@@ -9,7 +9,7 @@ public class ObstacleSpawner : MonoBehaviour
   public float MinDistance;
   public float MaxDistance;
 
-  public GameObject[] Solids;
+  public ObstacleDefinition[] Obstacles;
   public GameObject RootBehavior;
 
   private SpeedController Speed;
@@ -19,7 +19,7 @@ public class ObstacleSpawner : MonoBehaviour
   {
     Random.InitState(Seed);
     Speed = GameObject.FindObjectOfType<SpeedController>();
-    Distance = 0f;
+    Distance = Mathf.Abs(Height / 2f);
   }
 
   void Update()
@@ -27,10 +27,12 @@ public class ObstacleSpawner : MonoBehaviour
     Distance += Speed.Speed * Time.deltaTime;
     if (Distance >= 0)
     {
+      var definition = Obstacles[Random.Range(0, Obstacles.Length)];
       var root = Instantiate(RootBehavior, new Vector3(0, Height + Distance, 0), Quaternion.identity);
-      var solid = Instantiate(Solids[Random.Range(0, Solids.Length)], root.transform);
-      solid.transform.localPosition = Random.insideUnitSphere;
-      solid.transform.localRotation = Quaternion.Euler(0, Random.value * 360f, 0);
+      root.transform.localRotation = Quaternion.Euler(0, Random.value * 360f, 0);
+      var solid = Instantiate(definition.Prefab, root.transform);
+      solid.transform.localPosition = new Vector3(Random.Range(-1, 1) * definition.MaxTranslation.x, 0, Random.Range(-1, 1) * definition.MaxTranslation.y);
+      solid.transform.localScale = new Vector3(1, 1, 1) * Random.Range(definition.MinScale, definition.MaxScale);
       Distance -= Random.Range(MinDistance, MaxDistance);
     }
   }
---
+++ Assets/Obstacles/Scripts/ObstacleDefinition.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ObstacleDefinition
+{
+  public GameObject Prefab;
+  public Vector2 MaxTranslation;
+  public float MinScale;
+  public float MaxScale;
+}
